9a89f2c1379a8a051e841c207a344c89889bbe84
paulohgs <paulohgs@alu.ufc.br>
2022-05-11 09:52:18 -0300

    Initial Commit

---
+++ SweetWeather/SweetWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SweetWeatherApp.swift
+//  SweetWeather
+//
+//  Created by Paulo Henrique Gomes da Silva on 11/05/22.
+//
+
+import SwiftUI
+
+@main
+struct SweetWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
