c8b01d597312b9fa0b00d2cb504e3c666234bc3d
Joan Wilson <joanwmoliveira@gmail.com>
2022-10-01 11:41:11 -0300

    feat: solving swiftlint warnings and removing the duplicated navigation controller

--- SweetWeather/FavoritesView.swift
+++ SweetWeather/FavoritesView.swift
@@ -8,45 +8,43 @@
 import SwiftUI
 
 struct FavoritesView: View {
+
     @State private var showingSheet: Bool = false
-    
+
     var body: some View {
-        
-        NavigationView {
-            ScrollView(.vertical) {
-                VStack {
-                    ForEach((1...5), id: \.self){_ in
-                        Button(action: {
-                            self.showingSheet = true
-                        }) {
-                            CardView(city: .citiesWeather)
-                                .overlay(
-                                    RoundedRectangle(cornerRadius: 10)
-                                        .stroke(Color("text-card"), lineWidth: 1)
-                                )
-                                .frame(height: 80)
-                                .padding()
-                        }.sheet(isPresented: $showingSheet) {
-                            NavigationView {
-                                Text("Arrasta pra baixo!!!")
-                                    .toolbar {
-                                        ToolbarItem(placement: .primaryAction) {
-                                            Button(action: {
-                                                self.showingSheet = false
-                                            }) {
-                                                Text("Done").fontWeight(.semibold)
-                                            }
+        ScrollView(.vertical) {
+            VStack {
+                ForEach((1...5), id: \.self) { _ in
+                    Button {
+                        self.showingSheet = true
+                    } label: {
+                        CardView(city: .citiesWeather)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 10)
+                                    .stroke(Color("text-card"), lineWidth: 1)
+                            )
+                            .frame(height: 80)
+                            .padding()
+                    }.sheet(isPresented: $showingSheet) {
+                        NavigationView {
+                            Text("Arrasta pra baixo!!!")
+                                .toolbar {
+                                    ToolbarItem(placement: .primaryAction) {
+                                        Button("Done", role: .destructive) {
+                                            self.showingSheet = false
                                         }
+
                                     }
-                            }
+                                }
                         }
                     }
+
                 }
             }
-            .navigationTitle("Favorites Places")
-//            .navigationBarTitleDisplayMode(.inline)
         }
+        .navigationTitle("Favorites Places")
     }
+
 }
 
 struct FavoritesView_Previews: PreviewProvider {
