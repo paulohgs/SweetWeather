8b3e6c5b81447cabe9b66add093891070366c074
Joan Wilson <joanwmoliveira@gmail.com>
2022-10-01 11:41:11 -0300

    feat: solving swiftlint warnings and removing the duplicated navigation controller

--- SweetWeather/FavoritesView.swift
+++ SweetWeather/FavoritesView.swift
@@ -8,36 +8,50 @@
 import SwiftUI
 
 struct FavoritesView: View {
+
     @State var showingSheet: Bool = false
-    
+
     var body: some View {
-        
-        NavigationView {
-            ScrollView(.vertical) {
-                VStack {
-                    ForEach((1...5), id: \.self){_ in
-                        Button(action: {
-                            self.showingSheet = true
-                        }) {
-                            CardView(city: .citiesWeather)
-                                .overlay(
-                                    RoundedRectangle(cornerRadius: 10)
-                                        .stroke(Color("text-card"), lineWidth: 1)
-                                )
-                                .frame(height: 80)
-                                .padding()
-                        }.sheet(isPresented: $showingSheet) {
-                            FavoritesItemView(showingSheet: $showingSheet)
-                            }
+        ScrollView(.vertical) {
+            VStack {
+                ForEach((1...5), id: \.self) { _ in
+                    Button {
+                        self.showingSheet = true
+                    } label: {
+                        CardView(city: .citiesWeather)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 10)
+                                    .stroke(Color("text-card"), lineWidth: 1)
+                            )
+                            .frame(height: 80)
+                            .padding()
+                    }.sheet(isPresented: $showingSheet) {
+                        // FavoritesItemView(showingSheet: $showingSheet)
+                        NavigationView {
+                            Text("Arrasta pra baixo!!!")
+                                .toolbar {
+                                    ToolbarItem(placement: .primaryAction) {
+                                        Button("Done", role: .destructive) {
+                                            self.showingSheet = false
+                                        }
+
+                                    }
+                                }
                         }
                     }
+
                 }
             }
-            .navigationTitle("Favorites Places")
-//            .navigationBarTitleDisplayMode(.inline)
         }
+        .navigationTitle("Favorites Places")
     }
 
+}
+
+<<<<<<< HEAD
+=======
+}
+>>>>>>> c8b01d5 (feat: solving swiftlint warnings and removing the duplicated navigation controller)
 
 struct FavoritesView_Previews: PreviewProvider {
     static var previews: some View {
